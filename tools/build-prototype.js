325d4fd10c5d5e80318194a551d94aa0cd95406a
AndrewVerhoturov1 <andrew.verhoturov@mail.ru>
2026-05-23 18:40:27 +0500

    bootstrap: build-prototype.js

---
+++ tools/build-prototype.js
@@ -0,0 +1,3 @@
+// Individual prototype builder stub.
+// Full catalog builds are handled by build-pages-catalog.js.
+console.log('[build-prototype] Use build-pages-catalog.js for full catalog builds.');
